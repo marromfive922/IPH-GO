95a738a7d61a1ee1e4bf8f0b0a8bf46823fffc97
marromfive922 <148595083+marromfive922@users.noreply.github.com>
2026-04-24 01:03:56 +0100

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,37 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Exams from "./pages/Exams";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path={"/"} component={Home} />
+      <Route path={"/quiz"} component={Quiz} />
+      <Route path={"/exams"} component={Exams} />
+      <Route path={"/404"} component={NotFound} />
+      {/* Final fallback route */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="light">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
